027bf8c81f459ba03108382591d2c0d8225d9235
marcosschlick <marcosschlick@gmail.com>
2024-11-03 10:22:15 -0300

    first version

---
+++ src/test/java/me/dio/bootcamp_claro_dio/BootcampClaroDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bootcamp_claro_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampClaroDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
